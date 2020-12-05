e1573dfa6b0040c6356b09b6303e8a71d207b531
Ravineel <rajatsinghi89@gmail.com>
2020-12-05 18:54:53 +0530

    front -end paricles

--- src/App.js
+++ src/App.js
@@ -3,17 +3,56 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOption = {
+  particles: {
+    number:{
+      value:60,
+      density :{
+        enable:true,
+        value_area:800
+      }
+    }  
+  }
+}
+
+
+
 class App extends Component{
 
+  constructor(){
+    super();
+    this.state={
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+
+    console.log(event.target.value)
+
+  }
+
+  onButtonSubmit = () =>{
+
+    console.log('click')
+
+  }
 
   render(){
 
     return (
 
       <div className='App'>
+          <Particles className="particles"
+              params={particlesOption}/>
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+      
         {/* <FaceRecognition /> */}
       </div>
 
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
-const ImageLinkform = () => {
+
+const ImageLinkform = ({ onInputChange, onButtonSubmit }) => {
 
     return (
 
@@ -10,8 +11,8 @@ const ImageLinkform = () => {
             </p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input type='text' className="f4 pa2 w-70 center"/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
+                    <input type='text' className="f4 pa2 w-70 center" onChange={onInputChange}/>
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>
         </div>
