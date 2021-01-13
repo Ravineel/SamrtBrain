3960b10373b33ea8e7acdde367db8333339526b2
Ravineel <rajatsinghi89@gmail.com>
2021-01-13 17:00:07 +0530

    c

--- src/App.js
+++ src/App.js
@@ -42,11 +42,6 @@ class App extends Component{
     }
   }
 
-  componentDidMount(){
-    fetch('http://localhost:3000/')
-    .then(response => response.json())
-    .then(console.log)
-  }
   calculateFaceLocation = (data) =>{
 
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -87,7 +82,6 @@ class App extends Component{
 
 
   onRouteChange = (route) =>{
-
     if(route === 'signin'){
       this.setState({isSignedIn:false})
     }
